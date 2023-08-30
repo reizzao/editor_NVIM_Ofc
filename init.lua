2cea58904878fc7155b502c4b55c908ce365f07e
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-08-30 14:56:38 -0300

    feat: nvim in use rzj

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("plugins.plugins")
+require("configs.settings")
+require("configs.setUX")
+require("configs.setsTheme")
+require("maps.mapsEditions")
+require("maps.mapsFunctions")
+require("maps.mapsPlugins")
+require("maps.mapsTerminal")
+-- require("plugins.utils")  -- TODO - esta dando erro
+require("plugins.complete")
+require("plugins.news")
+
